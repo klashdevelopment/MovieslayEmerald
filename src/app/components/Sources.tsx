ae3f7ae59f3a899501a6c4a29276556c6477163c
Gavin Fox <gavingogaming@gmail.com>
2026-06-29 02:54:04 -0700

    Control logos + vyla source

--- src/app/components/Sources.tsx
+++ src/app/components/Sources.tsx
@@ -3,57 +3,77 @@ export const sources: {
         series: string;
         movie: string;
         tooltip: string;
+        tags?: string[];
+        icon?: string;
     };
 } = { // first 4 are displayed.
     "movieslay": {
         "series": "/player/%HOME_DATA%",
         "movie": "/player/%HOME_DATA%",
-        "tooltip": "Movieslay (Best, ad-free, beta)"
+        "tooltip": "Movieslay (Best, ad-free, beta)",
+        "tags": ["recommended", "ad-free", "beta"],
+        "icon": "/assets/source_icon.png"
+    },
+    "vyla": { // could move to vyla.cc soon
+        "series": "https://1c34-fuckmarkscan.hf.space/?id=%id%&season=%sid%&episode=%eid%",
+        "movie": "https://1c34-fuckmarkscan.hf.space/?id=%id%",
+        "tooltip": "Vyla (partner, ad-free)",
+        "tags": ["recommended", "partner", "ad-free"],
+        "icon": "/assets/vyla-api-logo.png"
     },
     "111movies": {
         "series": "https://111movies.net/tv/%id%/%sid%/%eid%",
         "movie": "https://111movies.net/movie/%id%",
-        "tooltip": "111Movies (New, beta)"
+        "tooltip": "111Movies (New, beta)",
+        "tags": ["new", "beta", "ads"]
     },
     "vidcore": {
         "series": "https://vidcore.net/tv/%id%/%sid%/%eid%",
         "movie": "https://vidcore.net/movie/%id%",
-        "tooltip": "VidCore (Regular)"
+        "tooltip": "VidCore (Regular)",
+        "tags": ["regular", "ads"]
     },
     "videasy": {
         "series": "https://player.videasy.net/tv/%id%/%sid%/%eid%",
         "movie": "https://player.videasy.net/movie/%id%",
-        "tooltip": "Videasy (Regular)"
+        "tooltip": "Videasy (Regular)",
+        "tags": ["regular", "ads"]
     },
     "vidapi": {
         "series": "https://vaplayer.ru/embed/tv/%id%/%sid%/%eid%?color=%23615fff",
         "movie": "https://vaplayer.ru/embed/movie/%id%?color=%23615fff",
-        "tooltip": "VidAPI (Regular)"
+        "tooltip": "VidAPI (Regular)",
+        "tags": ["regular", "ads"]
     },
     "vsrc2": {
         "series": "https://vidsrc.cc/v2/embed/tv/%id%/%sid%/%eid%?autoPlay=true",
         "movie": "https://vidsrc.cc/v2/embed/movie/%id%?autoPlay=true",
-        "tooltip": "VSrc2 (Great)"
+        "tooltip": "VSrc2 (Great)",
+        "tags": ["regular", "hd", "ads"]
     },
     "2embed": {
         "series": "https://www.2embed.cc/embedtv/%id%&s=%sid%&e=%eid%",
         "movie": "https://www.2embed.cc/embed/%id%",
-        "tooltip": "2Embed (Regular)"
+        "tooltip": "2Embed (Regular)",
+        "tags": ["regular", "ads"]
     },
     "vidsrc": {
         "series": "https://vsembed.ru/embed/tv?tmdb=%id%&season=%sid%&episode=%eid%",
         "movie": "https://vsembed.ru/embed/movie?tmdb=%id%",
-        "tooltip": "VSrc1 (Regular)"
+        "tooltip": "VSrc1 (Regular)",
+        "tags": ["regular", "ads"]
     },
     "vidrock": {
         "series": "https://vidrock.ru/tv/%id%/%sid%/%eid%?autoplay=true&theme=%23615fff&nextbutton=false&episodeselector=false",
         "movie": "https://vidrock.ru/movie/%id%?autoplay=true&theme=%23615fff",
-        "tooltip": "VidRock (Regular)"
+        "tooltip": "VidRock (Regular)",
+        "tags": ["regular", "ads"]
     },
     "smashy": {
         "series": "https://player.smashystream.com/tv/%id%?s=%sid%&e=%eid%",
         "movie": "https://player.smashystream.com/movie/%id%",
-        "tooltip": "Smashy (Bad popups)"
+        "tooltip": "Smashy (Bad popups)",
+        "tags": ["regular", "slow", "ads"]
     },
 };
 export default sources;
--- src/app/components/controls.tsx
+++ src/app/components/controls.tsx
@@ -5,23 +5,23 @@ import sources from "./Sources";
 export default function Controls({
     fullscreen,
     setFullscreen,
-    source=null,
-    setSource=null,
+    source = null,
+    setSource = null,
     isMovie,
-    nextEpisode=()=>{},
-    lastEpisode=()=>{},
-    onCustomServerChange=null,
-    customServerNumber=0,
+    nextEpisode = () => { },
+    lastEpisode = () => { },
+    onCustomServerChange = null,
+    customServerNumber = 0,
 }: {
     fullscreen: boolean;
     setFullscreen: any;
-    source?: (keyof typeof sources)|null;
-    setSource?: any|null;
+    source?: (keyof typeof sources) | null;
+    setSource?: any | null;
     isMovie: boolean;
     nextEpisode?: any;
     lastEpisode?: any;
-    onCustomServerChange?: ((amount: number) => void)|null;
-    customServerNumber?: string|number;
+    onCustomServerChange?: ((amount: number) => void) | null;
+    customServerNumber?: string | number;
 }) {
     return <>
         <button className="server" onClick={() => {
@@ -44,7 +44,7 @@ export default function Controls({
         {onCustomServerChange && (
             <div className="server light">
                 <div className="server-split">
-                    <div className="ss-item left-corners" onClick={()=>{
+                    <div className="ss-item left-corners" onClick={() => {
                         onCustomServerChange(-1);
                     }}>
                         <i className="fa-solid fa-backward"></i>
@@ -52,7 +52,7 @@ export default function Controls({
                     <div className="ss-item not-button" style={{ width: '100%' }}>
                         {customServerNumber}
                     </div>
-                    <div className="ss-item right-corners" onClick={()=>{
+                    <div className="ss-item right-corners" onClick={() => {
                         onCustomServerChange(1);
                     }}>
                         <i className="fa-solid fa-forward"></i>
@@ -64,32 +64,16 @@ export default function Controls({
             <div className="server light">
                 <div className="server-split">
                     {Object.keys(sources).slice(0, 4).map((key, i) => (
-                        <Tooltip key={key} title={sources[key].tooltip} className={`ss-item ${key==='movieslay' && 'movieslaysource'}
-                            ${i===3 && 'right-corners'}
-                            ${i===0 && 'left-corners'} ${source == key && 'active'}`} onClick={() => {
-                            setSource(key);
-                        }}>
-                            {key!=='movieslay' ? <i className={`fa-solid fa-dice-${['one', 'two', 'three', 'four'][i]}`}></i> :
-                                <i>
-                                    <img src="/assets/source_icon.png" style={{width:'25px',height:'25px',margin:'0 auto'}} />
-                                </i>
+                        <Tooltip key={key} title={sources[key].tooltip} className={`ss-item ${key === 'movieslay' && 'movieslaysource'}
+                            ${i === 3 && 'right-corners'}
+                            ${i === 0 && 'left-corners'} ${source == key && 'active'}`} onClick={() => {
+                                setSource(key);
+                            }}>
+                            {!!sources[key].icon ? <i>
+                                <img src={sources[key].icon} style={{ width: '25px', height: '25px', margin: '0 auto' }} />
+                            </i> : <i className={`fa-solid fa-dice-${['one', 'two', 'three', 'four'][i]}`}></i>
                             }
                         </Tooltip>
-                        // <Tooltip title="2Embed (Third best)" className={`ss-item ${source == '2embed' && 'active'}`} onClick={() => {
-                        //     setSource('2embed');
-                        // }}>
-                        //     <i className="fa-solid fa-dice-two"></i>
-                        // </Tooltip>
-                        // <Tooltip title="Smashy (Bad popups)" className={`ss-item ${source == 'smashy' && 'active'}`} onClick={() => {
-                        //     setSource('smashy');
-                        // }}>
-                        //     <i className="fa-solid fa-dice-three"></i>
-                        // </Tooltip>
-                        // <Tooltip title="VSrc2 (Reccomended)" className={`ss-item right-corners ${source == 'vsrc2' && 'active'}`} onClick={() => {
-                        //     setSource('vsrc2');
-                        // }}>
-                        //     <i className="fa-solid fa-dice-four"></i>
-                        // </Tooltip>
                     ))}
                 </div>
             </div>
